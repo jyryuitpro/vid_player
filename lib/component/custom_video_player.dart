968652d3012013a8b723db8f3f588cd3bd26e4e1
jyr <jyr@wavers.co.kr>
2022-06-19 15:29:00 +0900

    동영상 위에 버튼들 올리기

--- lib/component/custom_video_player.dart
+++ lib/component/custom_video_player.dart
@@ -42,8 +42,124 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
       return CircularProgressIndicator();
     }
 
-    return VideoPlayer(
-      videoPlayerController!,
+    return AspectRatio(
+      aspectRatio: videoPlayerController!.value.aspectRatio,
+      child: Stack(
+        children: [
+          VideoPlayer(
+            videoPlayerController!,
+          ),
+          _Controls(
+            onReversePressed: onReversePressed,
+            onPlayPressed: onPlayPressed,
+            onForwardPressed: onForwardPressed,
+            isPlaying: videoPlayerController!.value.isPlaying,
+          ),
+          Positioned(
+            right: 0,
+            child: IconButton(
+              onPressed: () {},
+              color: Colors.white,
+              iconSize: 30.0,
+              icon: Icon(Icons.photo_camera_back),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void onReversePressed() {
+    final currentPosition = videoPlayerController!.value.position;
+
+    // 기본 0초
+    Duration position = Duration();
+
+    if (currentPosition.inSeconds > 3) {
+      position = currentPosition - Duration(seconds: 3);
+    }
+
+    videoPlayerController!.seekTo(position);
+  }
+
+  void onPlayPressed() {
+    // 이미 실행중이면 중지
+    // 실행중이 아니면 실행
+    setState(() {
+      if (videoPlayerController!.value.isPlaying) {
+        videoPlayerController!.pause();
+      } else {
+        videoPlayerController!.play();
+      }
+    });
+  }
+
+  void onForwardPressed() {
+    final maxPosition = videoPlayerController!.value.duration;
+    final currentPosition = videoPlayerController!.value.position;
+
+    // 기본 0초
+    Duration position = maxPosition;
+
+    if ((maxPosition - Duration(seconds: 3)).inSeconds > currentPosition.inSeconds) {
+      position = currentPosition + Duration(seconds: 3);
+    }
+
+    videoPlayerController!.seekTo(position);
+
+  }
+}
+
+class _Controls extends StatelessWidget {
+  final VoidCallback onReversePressed;
+  final VoidCallback onPlayPressed;
+  final VoidCallback onForwardPressed;
+  final bool isPlaying;
+
+  const _Controls({
+    required this.onReversePressed,
+    required this.onPlayPressed,
+    required this.onForwardPressed,
+    required this.isPlaying,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black.withOpacity(0.5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          renderIconButton(
+            onPressed: onReversePressed,
+            iconData: Icons.rotate_left,
+          ),
+          renderIconButton(
+            onPressed: onPlayPressed,
+            iconData: isPlaying ? Icons.pause : Icons.play_arrow,
+          ),
+          renderIconButton(
+            onPressed: onForwardPressed,
+            iconData: Icons.rotate_right,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget renderIconButton({
+    required VoidCallback onPressed,
+    required IconData iconData,
+  }) {
+    return IconButton(
+      onPressed: onPressed,
+      iconSize: 30.0,
+      color: Colors.white,
+      icon: Icon(
+        iconData,
+      ),
     );
   }
 }
