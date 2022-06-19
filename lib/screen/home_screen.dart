14e83a76f67351797f18dfc567b1472a28f7bed4
jyr <jyr@wavers.co.kr>
2022-06-19 14:18:03 +0900

    ImagePicker 사용해보기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:vid_player/component/custom_video_player.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -20,7 +21,9 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget renderVideo() {
     return Center(
-      child: Text('Video'),
+      child: CustomVideoPlayer(
+        video: video!,
+      ),
     );
   }
 
